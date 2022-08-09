33b8c666926814037fa272d228dd081b5e62590e
Anton-L-Ext <anton.lorge@extendaretail.com>
2022-08-09 16:52:21 +0200

    feature: Initial commit

---
+++ examples/main.tf
@@ -0,0 +1,5 @@
+module alert {
+  source                = "/Users/user/Desktop/gcp-monitoring/tf-module-gcp-monitoring/modules/alert"
+  monitoring_project_id = "hiiretail-monitoring-prod-6500"
+  policies              = yamldecode(file("alerts.yaml"))
+}
---
+++ vars.tf
@@ -0,0 +1,21 @@
+variable monitoring_project_id {
+  type        = string
+  description = "Project ID to create monitoring resources in"
+  default     = "hiiretail-monitoring-prod-6500"
+}
+variable policies {
+  type        = any
+  description = "List of configs for alert policies"
+}
+
+variable user_labels {
+  type        = map(any)
+  description = "Project ID to create monitoring resources in"
+  default     = {}
+}
+
+variable notification_channels {
+  type        = list(any)
+  description = "List of notificaton channel IDs"
+  default     = []
+}
