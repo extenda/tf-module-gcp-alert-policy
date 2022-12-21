899dc2761ee3fe814fe44c19058989073a3c0801
Anton-L-Ext <anton.lorge@extendaretail.com>
2022-12-21 14:44:17 +0100

    fix: Linting

--- vars.tf
+++ vars.tf
@@ -1,17 +1,17 @@
 variable project {
   type        = string
-  description = "Project ID to create monitoring resources in"
+  description = "Project ID to create the alerts in"
 }
 
 variable default_user_labels {
   type        = map(any)
-  description = "User labels set on all alerts"
+  description = "User labels to be set for all alerts"
   default     = {}
 }
 
 variable default_notification_channels {
   type        = list(any)
-  description = "Notificaton channel IDs set on all alerts"
+  description = "Notificaton channel IDs to be set on all alerts"
   default     = []
 }
 
