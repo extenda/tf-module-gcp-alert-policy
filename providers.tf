3cdda7c73588be29009389fb4f9d965e10b573b4
Anton-L-Ext <anton.lorge@extendaretail.com>
2024-01-30 14:25:18 +0100

    feat: Add support for new alert features

--- providers.tf
+++ providers.tf
@@ -1,7 +1,10 @@
 terraform {
   # The configuration for this backend will be filled in by Terragrunt
-  required_version = ">= 0.12.18"
+  required_version = ">= 0.13.0"
   required_providers {
-    google = ">= 4.0"
+    google = {
+      source = "hashicorp/google"
+      version = ">= 5.13.0"
+    }
   }
 }
